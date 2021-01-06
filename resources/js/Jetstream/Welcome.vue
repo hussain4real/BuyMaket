a0656549b5f155c826c23b83e51d0075cc98c51c
Hussain Aminu <hussain_aminu@yahoo.com>
2021-01-06 16:11:59 +0100

    few changes

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -5,7 +5,7 @@
         <h1
           class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
         >
-          <span class="block xl:inline">Data to enrich your</span>
+          <span class="block xl:inline">Marketplace to enrich</span>
           <span class="block text-indigo-600 xl:inline">online business</span>
         </h1>
         <p
